1f97110ddc84c666dd7169306f8430d9af468737
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-06-19 23:19:31 +0530

    REF: HomeScreenView UI

--- app/src/main/java/com/example/godeliveryapp/presentation/homeScreen/HomeScreenView.kt
+++ app/src/main/java/com/example/godeliveryapp/presentation/homeScreen/HomeScreenView.kt
@@ -34,9 +34,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -69,10 +66,6 @@ fun HomeScreenView(
 ) {
 
     val isLoading = viewModel.isLoading.collectAsState(initial = true).value
-
-    val textFieldValue by remember {
-        mutableStateOf("")
-    }
 
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
